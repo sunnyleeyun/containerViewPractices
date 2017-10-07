7798439f4ca9ee237aff7189270098c3c6b719e9
Sunny <sunnylee945@hotmail.com>
2017-10-07 14:48:09 +0800

    DetailViewController's tableview datasource

--- SettingPage/New Group/DetailViewController.swift
+++ SettingPage/New Group/DetailViewController.swift
@@ -9,41 +9,88 @@
 import UIKit
 import SnapKit
 
-class DetailViewController: UIViewController {
+public class DetailViewController: UIViewController {
   
   private var tableView: UITableView!
-  public enum section{
-    
+  public enum DetailViewSection: Int{
+    case ProductPic = 0
+    case ProductDet = 1
+    case ProductSRF = 2
+    case Other = 3
   }
   
-  lazy var box = UIView()
   
-  override func viewDidLoad() {
+  //lazy var box = UIView()
+  
+  public override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
     
-    box.backgroundColor = .yellow
-    self.view.addSubview(box)
-    box.snp.makeConstraints { (make) -> Void in
-      make.width.height.equalTo(50)
-      make.center.equalTo(self.view)
+    configureUI()
+    
+//    box.backgroundColor = .yellow
+//    self.view.addSubview(box)
+//    box.snp.makeConstraints { (make) -> Void in
+//      make.width.height.equalTo(50)
+//      make.center.equalTo(self.view)
+//    }
+  }
+  
+  
+  public override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  
+  private func configureUI(){
+    configureTableView()
+  }
+  private func configureTableView(){
+    tableView = UITableView(frame: self.view.bounds)
+    tableView.backgroundColor = .white
+    
+    // register
+    tableView.register(cellTypes: [ProductPictureTableViewCell.self, ProductDetailsTableViewCell.self, SizeRateFitTableViewCell.self, OtherTableViewCell.self])
+    
+    tableView.delegate = self
+    tableView.dataSource = self
+    
+    tableView.rowHeight = 80.0
+    
+    tableView.anchor(to: view)
+    
+  }
+  
+}
+// MARK: - UITableViewDelegate
+extension DetailViewController: UITableViewDelegate{
+  
+}
+
+// MARK: - UITableViewDataSource
+extension DetailViewController: UITableViewDataSource{
+  public func numberOfSections(in tableView: UITableView) -> Int {
+    return 4
+  }
+  public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    switch indexPath.section{
+      
+    case DetailViewSection.ProductPic.rawValue:
+      let cell = tableView.dequeueReusableCell(with: ProductPictureTableViewCell.self, for: indexPath)
+      return cell
+    case DetailViewSection.ProductDet.rawValue:
+      let cell = tableView.dequeueReusableCell(with: ProductDetailsTableViewCell.self, for: indexPath)
+      return cell
+    case DetailViewSection.ProductSRF.rawValue:
+      let cell = tableView.dequeueReusableCell(with: SizeRateFitTableViewCell.self, for: indexPath)
+      return cell
+    case DetailViewSection.Other.rawValue:
+      let cell = tableView.dequeueReusableCell(with: OtherTableViewCell.self, for: indexPath)
+      return cell
+    default: fatalError()
     }
   }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
 }
